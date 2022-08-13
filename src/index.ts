4cc50fc974becd51a628c7a2569f09336e032aa1
Rifki Jones <lgodden@gmail.com>
2022-08-13 18:22:12 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+import morgan from 'morgan';
+import { nanoid } from 'nanoid';
+
+const db = new PrismaClient({ log: ['error', 'info', 'query', 'warn'] });
+const genId = () => nanoid(16);
+
+const seedDatabase = async () => {
+  if ((await db.submission.count()) === 0) {
+    await db.submission.createMany({
+      data: [
+        {
+          id: genId(),
+          submitttedAt: new Date(),
+          data: {
+            name: 'Kevin wade',
+            twitter: 'kevinwwade',
+          },
+        },
+      ],
+    });
+  }
+};
+seedDatabase();
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/', async (req, res) => {
+  const posts = await db.submission.findMany();
+  res.json(posts);
+});
+
+const port = Number(process.env.PORT ?? 8080);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
